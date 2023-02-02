244c289a0e6cc8ae33f486ca30332512e40f2057
lucas leiro <lucasleiroa@gmail.com>
2023-02-01 22:13:30 -0300

    intento de hacer primera pagina de incicio

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const router = jsonServer.router("BaseDeDatos.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001;
 
+
 server.use(middlewares);
 server.use(router);
 server.listen(port, () => {
